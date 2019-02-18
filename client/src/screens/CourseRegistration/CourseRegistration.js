ef01fe2ea05f463449e02c685012c558eb309711
Christiana <31808930+christiaua@users.noreply.github.com>
2019-02-17 23:21:27 -0500

    Frontend refactoring (#25)
    * Added hocs (Higher order components) for HeaderBar, refactored directories for screens and added a Switch in the Router
    
    * fix tabbing
    
    * wip Login: fixed tabbing issue
    
    * re-attempt to fix login.js tabbing
    
    * still tabbing
    
    * Add prettier
    
    * Run prettier on whole code

---
+++ client/src/screens/CourseRegistration/CourseRegistration.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './CourseRegistration.css';
+import WithHeaderBar from '../../hocs/WithHeaderBar';
+
+class CourseRegistration extends Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="course-registration">
+          <div className="instruction">
+            Select the courses that you have already taken.
+          </div>
+          <div className="content">
+            <div className="selection-side" />
+            <div className="selected-side" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WithHeaderBar(CourseRegistration);
