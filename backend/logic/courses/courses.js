c90abd839e8417e060dd42fc17368fb2dc8ef51b
AlexLam1997 <x.ela24@hotmail.com>
2019-02-18 09:13:13 -0500

    PR changes

--- backend/logic/courses/courses.js
+++ backend/logic/courses/courses.js
@@ -4,6 +4,7 @@ async function queryCourseByTag(tag){
     //if an invalid tag is passed, an empty object is returned
     let connection = await mysql.getNewConnection();
     let courses = await connection.query('SELECT course_code FROM course_tag WHERE course_tag.tag_name LIKE ?', tag);
+    await connection.release();
     return (JSON.parse(JSON.stringify(courses)));
 }
 //queryCourseByTag('Engineering').then(response => console.log(JSON.parse(JSON.stringify(response))));
