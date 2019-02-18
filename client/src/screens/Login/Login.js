6b88be58efae8a60ee657484b5d65532e99d50b9
Christiana <christiaua@outlook.com>
2019-02-17 19:45:49 -0500

    wip Login: fixed tabbing issue

--- client/src/screens/Login/Login.js
+++ client/src/screens/Login/Login.js
@@ -1,23 +1,28 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "./Login.css"
+import Section from "../../components/Section";
+import LoginForm from "../../components/LoginForm"
 
 class Login extends Component {
+	state = {
+		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+	}
 	render() {
 		return (
-			<div className="login">
-				<Fragment>
-					<div className="header">
-						<div className="title"> Project Athena</div>
-					</div>
-				</Fragment>
+			<Section flexDirection="column" justifyContent="space-between" style={{ height: "100vh" }}>
+				<Section className="header">
+					<Section className="title">Project Athena</Section>
+				</Section>
+				<Section className="content" justifyContent="space-around" flexDirection="row" alignItems="center">
+					<Section className="description">
+						{this.state.text}
+					</Section>
+					<Section className="form">
 
-				<div className="content">
-					<div className="description">
-					</div>
-					<div className="form">
-					</div>
-				</div>
-			</div>
+					</Section>
+				</Section>
+				<Section className="footer" />
+			</Section>
 		);
 	}
 }
