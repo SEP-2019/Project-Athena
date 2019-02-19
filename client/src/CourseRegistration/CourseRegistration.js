e09028621c9b2329e531bd93c1024403d66a7e51
Fiona Hang <fiona.hang@mail.mcgill.ca>
2019-02-19 15:19:58 -0500

    layout of course registration page

--- client/src/CourseRegistration/CourseRegistration.js
+++ client/src/CourseRegistration/CourseRegistration.js
@@ -1,21 +1,55 @@
 import React, { Component } from "react";
+import CourseRegistrationItem from './CourseRegistrationItem';
 import "./CourseRegistration.css";
 
 class CourseRegistration extends Component {
-  render() {
-    return (
-      <div className="course-registration">
-        <div className="instruction">Select the courses that you have already taken.</div>
-        <div className="content">
-            <div className="selection-side">
-            </div>
-            <div className="selected-side">
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchValue: "",
+            searchResults: [],
+            semester: "Fall 2017",
+            // temp data
+            selectedCourses: [
+                { 'course_code': 'COMP 202', 'title': 'Foundations of Programming', 'semester': 'Fall 2017' },
+                { 'course_code': 'MATH 262', 'title': 'Intermediate Calculus', 'semester': 'Winter 2017' },
+                { 'course_code': 'MATH 263', 'title': 'ODEs for Engineers', 'semester': 'Fall 2020' },
+                { 'course_code': 'COMP 250', 'title': 'Intro to Computer Science', 'semester': 'Winter 2020' },
+            ]
+        }
+    }
+
+    render() {
+        return (
+        <div className="course-registration">
+            <div className="instruction">Select the courses that you have already taken.</div>
+            <div className="content">
+                <div className="selection-side">
+                    <div className="selection-semester">
+                        <div>Choose the semester: </div>
+                        <div className="selection-dropdown">Fall 2017</div>
+                    </div>
+                    <div className="selection-course">
+                        <input type="text" className="selection-search"/>
+                        <button className="selection-button">Select</button>
+                    </div>
+                </div>
+                <ul className="selected-side">
+                    {
+                        this.state.selectedCourses.map((el, index) => (
+                            <CourseRegistrationItem 
+                                key={ index }
+                                number={ el.course_code } 
+                                title={ el.title }
+                                semester={ el.semester }
+                            />                            
+                        ))
+                    }
+                </ul>
             </div>
         </div>
-
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default CourseRegistration;
