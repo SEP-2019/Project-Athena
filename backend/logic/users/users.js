599481394e2f655dc65a64e220267d743e5d4c27
AlexLam1997 <x.ela24@hotmail.com>
2019-02-20 16:45:25 -0500

    PR changes, moved env variable definition to work with database connection

--- backend/logic/users/users.js
+++ backend/logic/users/users.js
@@ -1,94 +1,144 @@
 const mysql = require('../../sql/connection');
 const format = require('../../validation/format');
 const hasher = require('../../validation/hash');
+const util = require("util");
 
-exports.insertStudentUser = (username, password, email, id) => {
-	return new Promise(function(res, rej) {
-		// Connect to database
-		let promise = new Promise(function(resolve, reject) {
-			mysql.pool.getConnection(function(err, conn) {
-				if (err) { reject(err); }
-				resolve(conn);
-			});
-		});
+var insertStudentUser = (username, password, email, id) => {
+  return new Promise(function(res, rej) {
+    // Connect to database
+    let promise = new Promise(function(resolve, reject) {
+      mysql.pool.getConnection(function(err, conn) {
+        if (err) {
+          reject(err);
+        }
+        resolve(conn);
+      });
+    });
 
-		// Await for connection
-		promise.then(function(value) {
-			let connection = value;
-			let error;
+    // Await for connection
+    promise.then(
+      function(value) {
+        let connection = value;
+        let error;
 
-			// Check for missing input params
-			if (!username) {
-				error = 'undefined username';
-			} else if (!password) {
-				error = 'undefined password';
-			} else if (!email) {
-				error = 'undefined email';
-			} else if (!id) {
-				error = 'undefined id';
-			}
+        // Check for missing input params
+        if (!username) {
+          error = "undefined username";
+        } else if (!password) {
+          error = "undefined password";
+        } else if (!email) {
+          error = "undefined email";
+        } else if (!id) {
+          error = "undefined id";
+        }
 
-			if (error) {
-				connection.release();
-				console.error(error);
-				res(error);
-				return;
-			}
+        if (error) {
+          connection.release();
+          console.error(error);
+          res(error);
+          return;
+        }
 
-			// Check for invalid formatting
-			if (!format.verifyUsername(username)) {
-				error = 'invalid format username';
-			} else if (!format.verifyPassword(password)) {
-				error = 'invalid format password';
-			} else if (!format.verifyEmail(email)) {
-				error = 'invalid format email';
-			} else if (!format.verifyId(id)) {
-				error = 'invalid format id';
-			}
+        // Check for invalid formatting
+        if (!format.verifyUsername(username)) {
+          error = "invalid format username";
+        } else if (!format.verifyPassword(password)) {
+          error = "invalid format password";
+        } else if (!format.verifyEmail(email)) {
+          error = "invalid format email";
+        } else if (!format.verifyId(id)) {
+          error = "invalid format id";
+        }
 
-			if (error) {
-				connection.release();
-				console.error(error);
-				res(error);
-				return;
-			}
+        if (error) {
+          connection.release();
+          console.error(error);
+          res(error);
+          return;
+        }
 
-			// Hash the password
-			let hash = hasher.hashPass(password);
+        // Hash the password
+        let hash = hasher.hashPass(password);
 
-			// Begin transaction with database
-			connection.beginTransaction(function(error) {
-				if (error) { res(logError(connection, error)); return; }
-				// Test and insert into users table
-				connection.query('INSERT INTO users VALUES(?, ?, ?);',
-				                 [username, email, hash],
-				                 function (error, results, fields) {
-					if (error) { res(logError(connection, error)); return; }
-					// Test and insert into students table
-					connection.query('INSERT INTO students VALUES(?, ?);',
-					                 [id, username],
-					                 function (error, results, fields) {
-						if (error) { res(logError(connection, error)); return; }
-						// Commit transaction
-						connection.commit(function(error) {
-							if (error) { res(logError(connection, error)); return; }
-							connection.release();
-							res('true');
-						});
-					});
-				});
-			});
-		}, function(reason) {
-			console.error('Failed to establish connection to database');
-			console.error(reason);
-			res('false');
-		});
-	});
-}
+        // Begin transaction with database
+        connection.beginTransaction(function(error) {
+          if (error) {
+            res(logError(connection, error));
+            return;
+          }
+          // Test and insert into users table
+          connection.query(
+            "INSERT INTO users VALUES(?, ?, ?);",
+            [username, email, hash],
+            function(error, results, fields) {
+              if (error) {
+                res(logError(connection, error));
+                return;
+              }
+              // Test and insert into students table
+              connection.query(
+                "INSERT INTO students VALUES(?, ?);",
+                [id, username],
+                function(error, results, fields) {
+                  if (error) {
+                    res(logError(connection, error));
+                    return;
+                  }
+                  // Commit transaction
+                  connection.commit(function(error) {
+                    if (error) {
+                      res(logError(connection, error));
+                      return;
+                    }
+                    connection.release();
+                    res("true");
+                  });
+                }
+              );
+            }
+          );
+        });
+      },
+      function(reason) {
+        console.error("Failed to establish connection to database");
+        console.error(reason);
+        res("false");
+      }
+    );
+  });
+};
+
+var getCompletedCourses = async studentID => {
+  let courses = [];
+
+  const sql_query = `SELECT course_code, semester
+  		FROM course_offerings
+  		WHERE (id, semester)
+  		IN (SELECT offering_id, semester FROM student_course_offerings WHERE student_id = ?);`;
+
+  mysql.query = util.promisify(mysql.query);
+
+  let results;
+  try {
+    results = await mysql.query(sql_query, [studentID]);
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (results) {
+    courses = JSON.stringify(results);
+  }
+  return courses;
+};
 
 function logError(connection, error) {
-	console.error(error);
-	connection.rollback();
-	connection.release();
-	return 'false';
+  console.error(error);
+  connection.rollback();
+  connection.release();
+  return "false";
 }
+
+module.exports = {
+  insertStudentUser,
+  getCompletedCourses
+};
--- backend/test/users_test.js
+++ backend/test/users_test.js
@@ -1,7 +1,7 @@
-const mocha = require('mocha');
-const users = require('../logic/users/users.js');
-const assert = require('assert');
-const nock = require('nock');
+const mocha = require("mocha");
+const users = require("../logic/users/users.js");
+const assert = require("assert");
+const nock = require("nock");
 
 /*
 const host = 'http://localhost:3000';
@@ -42,224 +42,257 @@ describe('POST /users/addStudentUser', function() {
 });
 */
 
-describe('Tests add student user', function() {
-	it('responds with undefined username', function (done) {
-		users.insertStudentUser(null, 'password', 'email@email.com', '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, 'undefined username');
-					resolve();
-				}).then(done);
-		     	});
-	});
+describe("Tests add student user", function() {
+  it("responds with undefined username", function(done) {
+    users
+      .insertStudentUser(null, "password", "email@email.com", "123456789")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, "undefined username");
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with undefined password', function (done) {
-		users.insertStudentUser('username', null, 'email@email.com', '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, 'undefined password');
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with undefined password", function(done) {
+    users
+      .insertStudentUser("username", null, "email@email.com", "123456789")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, "undefined password");
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with undefined email', function (done) {
-		users.insertStudentUser('username', 'password', null, '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, 'undefined email');
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with undefined email", function(done) {
+    users
+      .insertStudentUser("username", "password", null, "123456789")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, "undefined email");
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with undefined id', function (done) {
-		users.insertStudentUser('username', 'password', 'email@email.com', null)
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, 'undefined id');
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with undefined id", function(done) {
+    users
+      .insertStudentUser("username", "password", "email@email.com", null)
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, "undefined id");
+          resolve();
+        }).then(done);
+      });
+  });
 
-	const expectedInvalidUsername = 'invalid format username';
-	it('responds with invalid format username 1', function (done) {
-		users.insertStudentUser('usernameWithSymbols123%@^', 'password', 'email@email.com', '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidUsername);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  const expectedInvalidUsername = "invalid format username";
+  it("responds with invalid format username 1", function(done) {
+    users
+      .insertStudentUser(
+        "usernameWithSymbols123%@^",
+        "password",
+        "email@email.com",
+        "123456789"
+      )
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidUsername);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with invalid format username 2', function(done) {
-		users.insertStudentUser('username123%@^', 'password', 'email@email.com', '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidUsername);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with invalid format username 2", function(done) {
+    users
+      .insertStudentUser(
+        "username123%@^",
+        "password",
+        "email@email.com",
+        "123456789"
+      )
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidUsername);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with invalid format username 3', function (done) {
-		users.insertStudentUser('RidiculouslyLongUsernameThatHasZeroPurposeToBeMadeAndAddedIntoTheDatabase',
-		                        'password', 'email@email.com', '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidUsername);
-					resolve();
-				}).then(done);
-		     	});
-	});
-	
-	const expectedInvalidPass = 'invalid format password';
-	it('responds with invalid format password 1', function(done) {
-		users.insertStudentUser('username', 'short', 'email@email.com', '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidPass);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with invalid format username 3", function(done) {
+    users
+      .insertStudentUser(
+        "RidiculouslyLongUsernameThatHasZeroPurposeToBeMadeAndAddedIntoTheDatabase",
+        "password",
+        "email@email.com",
+        "123456789"
+      )
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidUsername);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with invalid format password 2', function (done) {
-		users.insertStudentUser('username',
-		                        'thisIsARidiculouslyLongPasswordAndStuffButKeepGoingBecauseYeahSoDontDoThis',
-		                        'email',
-		                        '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidPass);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  const expectedInvalidPass = "invalid format password";
+  it("responds with invalid format password 1", function(done) {
+    users
+      .insertStudentUser("username", "short", "email@email.com", "123456789")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidPass);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	const expectedInvalidEmail = 'invalid format email';
-	it('responds with invalid format email 1', function (done) {
-		users.insertStudentUser('username',
-		                        'password',
-		                        'email',
-		                        '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidEmail);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with invalid format password 2", function(done) {
+    users
+      .insertStudentUser(
+        "username",
+        "thisIsARidiculouslyLongPasswordAndStuffButKeepGoingBecauseYeahSoDontDoThis",
+        "email",
+        "123456789"
+      )
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidPass);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with invalid format email 2', function (done) {
-		users.insertStudentUser('username',
-		                        'password',
-		                        'email#@gma.com',
-		                        '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidEmail);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  const expectedInvalidEmail = "invalid format email";
+  it("responds with invalid format email 1", function(done) {
+    users
+      .insertStudentUser("username", "password", "email", "123456789")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidEmail);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with invalid format email 3', function (done) {
-		users.insertStudentUser('username',
-		                        'password',
-		                        'email@g.c',
-		                        '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidEmail);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with invalid format email 2", function(done) {
+    users
+      .insertStudentUser("username", "password", "email#@gma.com", "123456789")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidEmail);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with invalid format email 4', function (done) {
-		users.insertStudentUser('username',
-		                        'password',
-		                        'email.2.2.12@@gmai.test.com',
-		                        '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidEmail);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with invalid format email 3", function(done) {
+    users
+      .insertStudentUser("username", "password", "email@g.c", "123456789")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidEmail);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	const expectedInvalidId = 'invalid format id';
-	it('responds with invalid format id 1', function (done) {
-		users.insertStudentUser('username',
-		                        'password',
-		                        'email@email.com',
-		                        '1234')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidId);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with invalid format email 4", function(done) {
+    users
+      .insertStudentUser(
+        "username",
+        "password",
+        "email.2.2.12@@gmai.test.com",
+        "123456789"
+      )
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidEmail);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with invalid format id 2', function (done) {
-		users.insertStudentUser('username',
-		                        'password',
-		                        'email@email.com',
-		                        '1234ABDS2')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidId);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  const expectedInvalidId = "invalid format id";
+  it("responds with invalid format id 1", function(done) {
+    users
+      .insertStudentUser("username", "password", "email@email.com", "1234")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidId);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with invalid format id 3', function (done) {
-		users.insertStudentUser('username',
-		                        'password',
-		                        'email@email.com',
-		                        'ab#%@a141')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, expectedInvalidId);
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with invalid format id 2", function(done) {
+    users
+      .insertStudentUser("username", "password", "email@email.com", "1234ABDS2")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidId);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with true 1', function(done) {
-		users.insertStudentUser('username1', 'password', 'email@email.com', '123456789')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, 'true');
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with invalid format id 3", function(done) {
+    users
+      .insertStudentUser("username", "password", "email@email.com", "ab#%@a141")
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, expectedInvalidId);
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with true 2', function (done) {
-		users.insertStudentUser('username2', 'passWITHsymbo!@#AOZ;]', 'email@email.com', '234567890')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, 'true');
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with true 1", function(done) {
+    users
+      .insertStudentUser(
+        "username1",
+        "password",
+        "email@email.com",
+        "123456789"
+      )
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, "true");
+          resolve();
+        }).then(done);
+      });
+  });
 
-	it('responds with true 3', function (done) {
-		users.insertStudentUser('username3', 'ASlightlyLongerPasswordThanNormal',
-		                        'I.Have.A.Really.Long.Email.Address@emailDomainToo.com', '535235231')
-			.then(response => {
-				return new Promise(function (resolve) {
-					assert.equal(response, 'true');
-					resolve();
-				}).then(done);
-		     	});
-	});
+  it("responds with true 2", function(done) {
+    users
+      .insertStudentUser(
+        "username2",
+        "passWITHsymbo!@#AOZ;]",
+        "email@email.com",
+        "234567890"
+      )
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, "true");
+          resolve();
+        }).then(done);
+      });
+  });
+
+  it("responds with true 3", function(done) {
+    users
+      .insertStudentUser(
+        "username3",
+        "ASlightlyLongerPasswordThanNormal",
+        "I.Have.A.Really.Long.Email.Address@emailDomainToo.com",
+        "535235231"
+      )
+      .then(response => {
+        return new Promise(function(resolve) {
+          assert.equal(response, "true");
+          resolve();
+        }).then(done);
+      });
+  });
 });
