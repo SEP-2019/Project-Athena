50e0a30296d168b6c1ce7e42735bfa2c2ad08549
AlexLam1997 <x.ela24@hotmail.com>
2019-02-19 22:25:27 -0500

    modified route to access pody of request

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 /* GET users listing. */
 router.post('/addStudentUser', function(req, res, next) {
-	let username = req.query.username;
-	let password = req.query.password;
-	let email = req.query.email;
-	let id = req.query.student_id;
+	let username = req.body.username;
+	let password = req.body.password;
+	let email = req.body.email;
+	let id = req.body.student_id;
 	users.insertStudentUser(username, password, email, id)
 	.then(function(val) {
 		console.log(val);
