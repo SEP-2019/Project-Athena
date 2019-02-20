83933ba97395c14f5c0d270e5c73a11154c804f5
Yu-Yueh Liu <yu-yueh@hotmail.com>
2019-02-19 23:47:43 -0500

    Add snackbar when applying or cancelling form, refactor checkbox component

--- client/src/components/Lists/InterestCheckBoxList.jsx
+++ client/src/components/Lists/InterestCheckBoxList.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './InterestCheckBoxList.css';
+import SmallCheckbox from '../CheckBoxes/Test/SmallCheckbox';
 
 class InterestCheckBoxList extends Component {
   constructor(props) {
@@ -27,61 +29,90 @@ class InterestCheckBoxList extends Component {
     }
   }
 
-  uncheckAll = uncheckAllEvent => {
+  uncheckAll = variant => uncheckAllEvent => {
     uncheckAllEvent.preventDefault();
     const { interests } = this.state;
     interests.forEach(interest => (interest.checked = false));
     this.setState({ interests });
+    let isCleared = false;
     for (const checkbox of this.selectedCheckboxes) {
       this.selectedCheckboxes.delete(checkbox);
       console.log(checkbox, 'is Cleared');
+      isCleared = true;
     }
+    if (isCleared) {
+      this.props.enqueueSnackbar('Your selections have been cleared', {
+        variant,
+      });
+    }
+    isCleared = false;
   };
 
-  handleFormSubmit = formSubmitEvent => {
+  handleFormSubmit = variant => formSubmitEvent => {
     formSubmitEvent.preventDefault();
 
     for (const checkbox of this.selectedCheckboxes) {
       console.log(checkbox, 'is Applied.');
+      this.props.enqueueSnackbar(checkbox + ' selection has been applied.', {
+        variant,
+      });
     }
   };
 
-  createCheckbox = (interest, index) => (
-    <div className="checkbox_area" key={index}>
-      <input
-        id={interest.name}
-        type="checkbox"
-        name={interest.name}
-        checked={interest.checked}
-        onChange={e =>
-          this.handleChange(index, e.target.checked, e.target.name)
-        }
-      />
-      <label for={interest.name} className="checkbox_label">
-        {interest.name}
-      </label>
-      <label for={interest.name} className="custom_checkbox" />
-    </div>
-  );
+  // createCheckbox = (interest, index) => (
+  //   <div className="checkbox_area" key={index}>
+  //     <input
+  //       id={interest.name}
+  //       type="checkbox"
+  //       name={interest.name}
+  //       checked={interest.checked}
+  //       onChange={e =>
+  //         this.handleChange(index, e.target.checked, e.target.name)
+  //       }
+  //     />
+  //     <label for={interest.name} className="checkbox_label">
+  //       {interest.name}
+  //     </label>
+  //     <label for={interest.name} className="custom_checkbox" />
+  //   </div>
+  // );
 
   render() {
     return (
-      <form className="interest_selection" onSubmit={this.handleFormSubmit}>
+      <form
+        className="interest_selection"
+        onSubmit={this.handleFormSubmit('success')}
+      >
         <h4>My Interests</h4>
         <div className="interest-list">
-          {this.props.interests.map(this.createCheckbox)}
+          {/* {this.props.interests.map(this.createCheckbox)} */}
+          {this.props.interests.map((interest, index) => (
+            <SmallCheckbox
+              key={index}
+              index={index}
+              name={interest.name}
+              checked={interest.checked}
+              handleChange={e =>
+                this.handleChange(index, e.target.checked, e.target.name)
+              }
+            />
+          ))}
         </div>
         <div className="btn_container">
+          <button className="btn" onClick={this.uncheckAll('info')}>
+            CANCEL
+          </button>
           <button className="btn" type="submit">
             APPLY
-          </button>
-          <button className="btn" onClick={this.uncheckAll}>
-            CANCEL
           </button>
         </div>
       </form>
     );
   }
 }
+
+InterestCheckBoxList.propTypes = {
+  interests: PropTypes.arrayOf(PropTypes.Object).isRequired,
+};
 
 export default InterestCheckBoxList;
