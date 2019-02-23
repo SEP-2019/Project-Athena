d5fbba24b3a659252298275b8960c23dcaa67e28
MatSavoie <mathieu.savoie3@mail.mcgill.ca>
2019-02-22 19:57:43 -0500

    Added route for adding a course

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -23,4 +23,50 @@ router.get('/getCourseByTag', async function (req, res, next) {
     }
 });
 
+
+/*
+* @api {get} /createCourse
+* @apiDescription This endpoint will add a course and an associated user
+* @apiParam (body) {string} courseCode, {string} title, {string} departement
+* @apiExample {curl} Example usage:
+* Http: 
+	POST /users/createCourse HTTP/1.1
+	Host: localhost:3000
+	Content-Type: application/json
+	{
+		"courseCode": "ECSE 428",
+		"title": "Software Engineering Practice",
+		"departement" : "ECSE"
+	}
+* Curl:
+	curl -X POST \
+	http://localhost:3000/users/createCourse\
+	-H 'Content-Type: application/json' \
+	-d '{
+		"courseCode": "ECSE 428",
+		"title": "Software Engineering Practice",
+		"departement" : "ECSE"
+	}'
+*
+* @returns true if course was added successfully or false otherwise
+*
+* @author: Mathieu Savoie
+*/
+
+router.post('/createCourse', function(req, res, next) {
+  const courseCode = req.body.courseCode;
+  const title = req.body.title;
+  const email = req.body.email;
+  const departement = req.body.departement;
+  users.insertStudentUser(title, email, departement)
+    .then(val => {
+      res.send(val);
+    })
+    .catch(err => {
+      res.status(500).send(err.message);
+    });
+});
+
+
 module.exports = router;
+
