dcf88131ab9905ddce216e4f93ba4b1abcc8f24b
Christiana <31808930+christiaua@users.noreply.github.com>
2019-03-22 01:25:38 -0400

    extract methods in sign up. use callbacks with setState

--- client/src/components/LoginForm/Validation.js
+++ client/src/components/LoginForm/Validation.js
@@ -1,26 +1,19 @@
 // Student ID should only contain 9 numeric characters
 export function isStudentId(id) {
-  return this.isEmpty(id) || !/^\d+$/.test(id) || id.length !== 9;
+  return !this.isEmpty(id) && /^\d+$/.test(id) && id.length === 9;
 }
 
 export function isEmpty(value) {
   return !value;
 }
 
-export function setError(message) {
-  document.getElementById('error-msg').innerHTML = message;
-}
-
-export function checkPassword(password) {
-  return password.length < 8 || password.length > 64;
-}
-
-export function checkLoginError(error) {
+export function isPassword(password) {
   return (
-    error === 'Password length must be between 8 and 64' ||
-    error === 'User does not exist' ||
-    error === 'Username length must be less than 64' ||
-    error === 'Username must be alphanumeric' ||
-    error === 'Incorrect username or password.'
+    !this.isEmpty(password) && password.length >= 8 && password.length <= 64
   );
+}
+
+export function isEmail(email) {
+  var validation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return !this.isEmpty(email) && validation.test(String(email).toLowerCase());
 }
