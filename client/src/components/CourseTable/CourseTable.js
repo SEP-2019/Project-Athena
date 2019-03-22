43250502c16d8a334e35289ce9397318413cc1a5
Patrick Lai <patricklai10123@gmail.com>
2019-03-21 21:47:41 -0400

    fix overwritten changes and add .Response to endpoint data fetch (#94)
    * fix overwritten changes and add .Response to endpoint data fetch
    
    * remove unused css
    
    * d

--- client/src/components/CourseTable/CourseTable.js
+++ client/src/components/CourseTable/CourseTable.js
@@ -1,66 +1,32 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
-const overrideStyle = {
-    color: '#FFFFFF',
-    fontSize: '0.8rem',
-    padding: '3px 0 3px 15px',
-};
-
-function createData(courseName, numCredits) {
-    return { courseName, numCredits };
-}
+import ExpandableCourse from '../ExpandableCourse/ExpandableCourse';
 
 class CourseTable extends Component {
     handleChange(event) {
         this.props.getValue(event.target.value);
-    }
-
-    constructor(props) {
+      }
+    
+      constructor(props) {
         super(props);
         this.state = {
-            courses: props.children,
-        }
-    }
-
-
-    render() {
+          courses: props.courses,
+        };
+      }
+    
+      render() {
         return (
-            // by default the table will span the width of the screen. Width should be
-            // specified by its parent element
-            <Paper>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Course</TableCell>
-                            <TableCell align="right">Number of Credits</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.props.courses.map((course) => (
-                            <TableRow key={course.courseName}>
-                                <TableCell>{course.courseName}</TableCell>
-                                <TableCell align="right">{course.numCredits}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Paper>
+          <div>
+            {this.state.courses.map((course, index) => (
+              <ExpandableCourse
+                key={index}
+                index={index}
+                course_code={course.course_code}
+                description={course.course_code}
+              />
+            ))}
+          </div>
         );
-    }
+      }
 }
-
-CourseTable.propTypes = {
-    courses: PropTypes.shape({
-        courseName: PropTypes.string,
-        numCredits: PropTypes.number,
-    }),
-};
 
 export default CourseTable;
