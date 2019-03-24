b659438dd8cd06ae0a861b5d83c79b39b690c32d
Patrick Lai <patricklai10123@gmail.com>
2019-03-23 22:34:04 -0400

    Patrick/feature/curriculum dropdown (#113)
    * add loading msg
    
    * add disable to dropdown
    
    * fix stuff
    
    * make it 6 courses per semester
    
    * pull from dev again
    
    * change css

--- client/src/screens/CurriculumDisplay/CurriculumDisplay.js
+++ client/src/screens/CurriculumDisplay/CurriculumDisplay.js
@@ -3,7 +3,6 @@ import './CurriculumDisplay.css';
 import Api from '../../services/Api'
 import WithHeaderBar from '../../hocs/WithHeaderBar';
 import CourseTable from '../../components/CourseTable';
-import axios from 'axios';
 import _ from 'lodash';
 import Cookies from 'universal-cookie';
 
@@ -14,8 +13,12 @@ class CurriculumDisplay extends Component {
     super(props);
     this.getStudentData = this.getStudentData.bind(this);
     this.parseCourseData = this.parseCourseData.bind(this);
-    this.getCourseTable = this.getCourseTable.bind(this);
+    this.renderCourseTable = this.renderCourseTable.bind(this);
+    this.renderLoadingMessage = this.renderLoadingMessage.bind(this);
+    this.renderInstructions = this.renderInstructions.bind(this);
     this.state = {
+      loadingMessage: "Loading curriculum information...",
+      instructions: "",
       studentId: null, // placeholder student id until sessions/persistance are implemented
       curriculumName: 'View ECSE Curriculums',
       completedCourses: [],
@@ -45,7 +48,7 @@ class CurriculumDisplay extends Component {
       .get('users/getStudentData?studentID=' + studentid)
       .then(response => {
 
-        let res = response.data.Response;
+        let res   = response.data.Response;
         
         this.setState({
           curriculumName: res.major[0].curriculum_name,
@@ -53,12 +56,24 @@ class CurriculumDisplay extends Component {
             res.completedCourses,
             'semester'
           ),
-          incompleteCourses: this.parseCourseData(
-            res.incompletedCore,
-            'semester'
-          ),
-          desiredTechComps: res.desiredTC,
-        });
+        }, () => {
+          // need to call set state again after since checking the prereqs for incomplete courses
+          // requires that completed courses is defined first
+          this.setState({
+            incompleteCourses: this.parseCourseData(
+              res.incompletedCore.concat(res.desiredTC),
+              'semester',
+              this.state.completedCourses.map(cc => cc.courses).flat(),
+            ),
+            desiredTechComps: this.parseCourseData(
+              res.desiredTC,
+              'semester',
+              this.state.completedCourses.map(cc => cc.courses).flat(),
+            ),
+            loadingMessage: "",
+            instructions: "Select the courses you wish to take in each semester from the tables below"
+          })
+        })
       })
       .catch(studentDataError => this.setState({ studentDataError }));
   }
@@ -70,10 +85,35 @@ class CurriculumDisplay extends Component {
    * @param {Object} data course data to be parsed
    * @param {string} key which key to group the data by
    */
-  parseCourseData(data, key) {
+  parseCourseData(data, key, completedCourses = []) {
     let group = _.groupBy(data, key);
 
     let courses = Object.keys(group).map(function(k) {
+      for(var i = 0; i < group[k].length; i++){
+        let hasPrereqs = false
+
+        // check if the student has the prereqs for the current course
+          try{
+            // if the prereqs are empty, assume the course has no prereqs and thus the
+            // student has all the requirements to take the course
+            // or else, check that the list of completed courses contains every prereq course
+            // for the current course
+            hasPrereqs = (group[k][i].prereqs === undefined || group[k][i].prereqs.length <= 0) ? true :
+            group[k][i].prereqs.every(prereq => {
+              completedCourses.some(c => c.course_code === prereq.prereq_course_code)
+            })
+          }
+          catch(error){
+            console.log(error)
+          }
+        
+        group[k][i].isDisabled = !hasPrereqs
+      }
+
+      if(group[k]){
+        group[k].map(course => course.displayMember = course.course_code + " - " + course.title)
+      }
+
       return { semester: k, courses: group[k] };
     });
 
@@ -85,12 +125,34 @@ class CurriculumDisplay extends Component {
    *
    * @param {Object} props properties to pass in. Should be in the form of {details: {semester: "W2019", courses: []}, mapFunction: f}
    */
-  getCourseTable(props) {
+  renderCourseTable(props) {
     // nothing is selected
-    if (!props.details || props.details.length === 0)
+    if (!props.details || props.details.length === 0){
       return <div>No courses found</div>;
+    }
+  
+    return <CourseTable 
+      courses={props.mapFunction(props.details.courses)}
+      useDropdown={props.useDropdown} 
+    />;
+  }
 
-    return <CourseTable courses={props.mapFunction(props.details.courses)} />;
+  /**
+   * Renders the loading message while the courses are being fetched
+   * 
+   * @param {Object} props 
+   */
+  renderLoadingMessage(props){
+    return props.message ? <div>{props.message}</div> : (null)
+  }
+
+  /**
+   * Renders the instructions to use the page
+   * 
+   * @param {Object} props 
+   */
+  renderInstructions(props){
+    return props.message ? <div className="instructions">{props.message}</div> : (null)
   }
 
   render() {
@@ -100,16 +162,19 @@ class CurriculumDisplay extends Component {
           <div className="page-header">
             {'Current Curriculum: ' + this.state.curriculumName}
           </div>
+          <this.renderLoadingMessage message = {this.state.loadingMessage}/>
+          <this.renderInstructions message = {this.state.instructions}/>
           <div className="curriculum-content">
             <div className="semester-course-display" key="Completed Courses">
-              {this.state.completedCourses.map(completedSemester => (
-                <div>
-                  <div className="semester-name">
+              {this.state.completedCourses.map((completedSemester, index) => (
+                <div key={"Complete__" + index}>
+                  <div className="semester-name" key={"Completed_" + index}>
                     {completedSemester.semester}
                   </div>
-                  <div className="semester-course-table" style={{ width: 512 }}>
-                    <this.getCourseTable
+                  <div className="semester-course-table" style={{ width: 512 }} key={"Completed_child_" + index}>
+                    <this.renderCourseTable
                       details={completedSemester}
+                      useDropdown={false}
                       typeOfCourses={'completedCourses'}
                       mapFunction={courses => courses}
                     />
@@ -119,14 +184,16 @@ class CurriculumDisplay extends Component {
             </div>
 
             <div className="semester-course-display" key="Incomplete Courses">
-              {this.state.incompleteCourses.map(incompleteSemester => (
-                <div>
-                  <div className="semester-name">
+            {/* make the selectable courses for then incomplete semesters the union of incomplete courses and desired TCs */}
+              {this.state.incompleteCourses.map((incompleteSemester, index) => (
+                <div key={"Incomplete__" + index}>
+                  <div className="semester-name" key={"Incomplete_" + index}>
                     {incompleteSemester.semester}
                   </div>
-                  <div className="semester-course-table" style={{ width: 512 }}>
-                    <this.getCourseTable
+                  <div className="semester-course-table" style={{ width: 512 }} key={"Incomplete_child" + index}>
+                    <this.renderCourseTable
                       details={incompleteSemester}
+                      useDropdown={true}
                       typeOfCourses={'incompleteCourses'}
                       mapFunction={courses => courses}
                     />
